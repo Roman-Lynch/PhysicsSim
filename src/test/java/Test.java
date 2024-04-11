5e6d3f60087921d5a31dee9f459b34bf35e89fd1
Roman-Lynch <roly7805@colorado.edu>
2024-04-10 18:47:39 -0600

    Created inital tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,16 @@
 public class Test {
+
+    Object obj = Object.newBuilder()
+            .shape()
+            .mass()
+            .velocity()
+            .direction()
+            .create();
+
+    Sim runSim = Sim.newBuilder()
+            .createAndAddMars(height, width)
+            .addObject(obj)
+            .addObject(obj)
+            .setRunTime(5)
+            .run();
 }
