9bd59a2516a57a32671490096131db2ee2b5b741
Roman-Lynch <roly7805@colorado.edu>
2024-04-10 19:35:43 -0600

    Added implementation to enviorment

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -1,2 +1,16 @@
 public class Environment {
+    protected double gravity;
+    protected double height;
+    protected double width;
+
+    public Environment(double gravity, double height, double width)
+    {
+        this.gravity = gravity;
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getGravity() { return gravity;}
+    public double getHeight() { return height;}
+    public double getWidth() { return width;}
 }
